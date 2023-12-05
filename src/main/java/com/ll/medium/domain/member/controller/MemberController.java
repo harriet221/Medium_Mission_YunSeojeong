36139950d22633c4bfe385b2b853fca1bfa2b349
harriet221 <potter19@dongguk.edu>
2023-12-05 21:45:12 +0900

    02 login/join form rename

--- src/main/java/com/ll/medium/domain/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ll.medium.domain.member.controller;
 import com.ll.medium.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -10,4 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+
+    @GetMapping("/join")
+    public String join() {
+        return "join_form";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
+
 }
